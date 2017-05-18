7899a9b28f3751afb1b6b35e06bafaf0e4df8078
MikelSage <michael.sagapolutele@yahoo.com>
2017-05-17 21:11:20 -0600

    modifies gui

--- complete_me_gui.rb
+++ complete_me_gui.rb
@@ -1,4 +1,6 @@
 require_relative './lib/complete_me'
+require 'shoes/videoffi'
+
 
 completion = CompleteMe.new
 dictionary = File.read("/usr/share/dict/words")
@@ -7,6 +9,9 @@ completion.populate(dictionary)
 Shoes.app(title: "CompleteMe",
    width: 750, height: 537, resizable: false) {
     background "./gui/completemevice.jpg"
+    image('http://www.freeiconspng.com/uploads/volume-icon-32.png', height: 50, width: 50) do
+      @v.stop
+    end
     flow do
       style(:margin_left => '55%', :left => '-25%', :margin_top => '75%')
       @edit = edit_line
@@ -24,10 +29,7 @@ Shoes.app(title: "CompleteMe",
         end
         @slot.style(:margin_left => '60%', :left => '-25%', :margin_top => '85%')
       end
-
-      button 'Clear' do
-        @slot.clear
-      end
     end
-    @v = video( "gui/billie-jean.mp3", :autoplay => true)
+    @v = video( "file:///Users/michaelsagapolutele/turing/1module/projects/complete_me/gui/Push_It_To_The_Limit.mp3",
+     :autoplay => true)
   }
