5d4dcfa1dc0be473cf4d1d3e4e96d5db2ee7cff5
MikelSage <michael.sagapolutele@yahoo.com>
2017-05-25 09:00:06 -0600

    updates test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 
-require 'minitest/autorun' 
+require 'minitest/autorun'
+require_relative 'complete_me_test'
+require_relative 'node_test'
+require_relative 'trie_test'
